7f932abb3de264a96c279aeca2d6e5257eec6079
Alessandro Lorenzi <alorenzi@alorenzi.eu>
2019-06-18 12:24:15 +0200

    Initial commit

---
+++ reddit-top-posts.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import requests
+
+from pprint import pprint 
+
+class TopPosts():
+    def __init__(self, subreddit):
+        self.subreddit = subreddit
+        self.retrive_posts()
+    
+    def retrive_posts(self):
+        headers = {'User-Agent': 'RedditTopPosts'}
+        url = "https://www.reddit.com/r/%s/top/.json?t=day.json" % self.subreddit
+        posts = requests.get(url, headers=headers).json()
+        self.top_posts = posts['data']['children']
+
+class BeautifyTopPosts():
+    
+    def __init__(self, subreddit, top_posts):
+        self.subreddit = subreddit
+        self.top_posts = top_posts
+
+    def markdown(self):
+        message = "# %s \n" % self.subreddit
+        message += "\n"
+        for post in self.top_posts:
+            message += "* [%s](https://reddit.com/%s)\n" % (post['data']['title'], post['data']['permalink'])
+        return message        
+
+if __name__ == '__main__':
+    for sub in ['netsec', 'devops']:
+        tp = TopPosts(sub)
+        bp = BeautifyTopPosts(sub, tp.top_posts)
+        print(bp.markdown())
+    
